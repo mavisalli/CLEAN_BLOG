ff3e6704c92dc0b98c6736dbfbf184922cdc0432
mavisalli <mavi.baris96@gmail.com>
2021-05-28 18:12:26 +0300

    prettier extension added

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-
+app.get("/", (req, res) => {
   const blog = {
     id: 1,
     title: "Blog title",
-    description: "Blog description"
-  }
-  res.send(blog)
-})
+    description: "Blog description",
+  };
+  res.send(blog);
+});
 
 const port = 3000;
 app.listen(port, () => {
