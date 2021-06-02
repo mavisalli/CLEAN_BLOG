8c6b3f077c8eb0baddb61c2268f1c510aef60e88
mavisalli <mavi.baris96@gmail.com>
2021-06-02 21:29:02 +0300

    mvc pattern created and upload,delete properties added

--- app.js
+++ app.js
@@ -1,67 +1,46 @@
 const mongoose = require("mongoose");
 const express = require("express");
 
+const fileUpload = require("express-fileupload");
 const ejs = require("ejs");
-const path = require("path");
+const methodOverride = require("method-override");
 
-const Post = require('./models/Post');
+
+const postController = require("./controllers/postController");
+const pageController = require("./controllers/pageController");
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
-
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
-
 //MIDDLEWARES
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true})); //body bilgisini yakalamak için 2 adet middleware fonksiyonunu kullanmamız gerekir.
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); //body bilgisini yakalamak için 2 adet middleware fonksiyonunu kullanmamız gerekir.
 app.use(express.json());
-
-
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-
-  res.render('index', {
-    posts
+app.use(fileUpload());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
   })
-});
+);
 
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render("post", {
-    post
-  })
+// ROUTES
+app.get("/", postController.getAllPosts);
+app.get("/posts/:id", postController.getPost);
+app.post("/posts", postController.createPost);
+app.put("/posts/:id", postController.updatePost);
+app.delete("/posts/:id", postController.deletePost);
 
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-
-});
-
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-
-});
-
-app.get("/post", (req, res) => {
-  res.render("post");
-
-});
-
-app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/")
-
-});
-
-
+app.get("/about", pageController.getAboutPage);
+app.get("/add_post", pageController.getAddPostPage);
+app.get("/posts/edit/:id", pageController.getEditPage);
 
 
 
